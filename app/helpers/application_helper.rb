c035664e2fa0a44919e50d460e7622753e0ae19b
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2020-01-02 00:57:38 -0300

    mail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def mensaje_image_tag(image)
 	attachments.inline[image] = File.read(Rails.root.join("public/#{image}"))
-    image_tag attachments[image].url, style: 'width:700px;height:auto;'
+    image_tag attachments[image].url, style: 'width:800px;height:auto;'
   end
 end
